ea8716b52957feba659ca49c1ef1c99a14f2dd99
zikripasya <ahmadzikri244@gmail.com>
2025-12-01 11:43:40 +0700

    Initialize Bus class with capacity and passenger lists
    Add constants and collections for bus capacity and passengers.

--- Bus.java
+++ Bus.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
+
+// Bus.java
+public class Bus {
+    public static final int ONGKOSBUS = 2000;
+    private int totalPendapatan = 0;
+
+    // batas kapasitas
+    public static final int MAX_BIASA = 16;
+    public static final int MAX_PRIORITAS = 4;
+    public static final int MAX_BERDIRI = 20;
+
+    // menyimpan penumpang saat runtime
+    private ArrayList<Penumpang> penumpangBiasa = new ArrayList<>();
+    private ArrayList<Penumpang> penumpangPrioritas = new ArrayList<>();
+    private ArrayList<Penumpang> penumpangBerdiri = new ArrayList<>();
 
